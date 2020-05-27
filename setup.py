92b9a9f754222e8bb16ca68df19f5bcbc439e1e9
osmancorluk <corluk@hotmail.com>
2020-05-27 11:47:57 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='pyrx-utils',
+   version='1.0',
+   description='reactive extesions',
+   author='Osman Corluk',
+   author_email='corluk@hotmail.com',
+   packages=['pyrx-utils'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
